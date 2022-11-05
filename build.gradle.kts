291d842fd46b672f0160972c210fd2440f316dbb
hope <hope@gravylab.co.kr>
2022-11-05 22:24:06 +0900

    change java version from 17 to 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,11 @@ plugins {
 group = "org.icloud"
 version = "1.0-SNAPSHOT"
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
 repositories {
     mavenCentral()
 }
